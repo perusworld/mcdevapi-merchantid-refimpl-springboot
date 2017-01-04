f62e042adb1de29be5f1f1ddc8862cda55474e1b
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2017-01-03 18:25:22 -0800

    rest controller def

--- src/main/java/com/yosanai/web/controller/MerchantIDController.java
+++ src/main/java/com/yosanai/web/controller/MerchantIDController.java
@@ -1,16 +1,16 @@
 package com.yosanai.web.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mastercard.api.core.model.RequestMap;
 import com.mastercard.api.merchantidentifier.MerchantIdentifier;
 import com.yosanai.web.model.MerchantIDRequest;
 
-@Controller
+@RestController
 public class MerchantIDController {
 
 	@RequestMapping(value = "/query", method = RequestMethod.POST, produces = "application/json")
